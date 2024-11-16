27e986f75574ba6176ee14236c37caf5c5469b84
a71143055 <139251375+a71143055@users.noreply.github.com>
2024-11-16 19:48:41 +0900

    chp06

--- src/chp06/NotificationList.jsx
+++ src/chp06/NotificationList.jsx
@@ -3,12 +3,15 @@ import Notification from "./Notification";
 
 const reservedNotification = [
     {
+        id:1,
         message: "안녕하세요~ 오늘 일정을 알려드립니다.",
     },
     {
+        id:2,
         message: "현재 수업중인 웹프로그래밍활용 수업을 열심히 들으세요.",
     },
     {
+        id:3,
         message: "열심히 듣고 Git에 Commit한 후 하교하시면 됩니다..",
     }
 ];
@@ -37,7 +40,7 @@ class NotificationList extends React.Component{
             }else{
                 clearInterval(timer);
             }
-        }, 1000);
+        }, 2000);
     }
 
     render() {
@@ -45,7 +48,10 @@ class NotificationList extends React.Component{
             <div>
                 {
                     this.state.notifications.map((notification) => {
-                        return <Notification message={notification.message}/>
+                        return <Notification
+                        key={notification.id}
+                        id={notification.id}
+                        message={notification.message}/>
                     })
                 }
             </div>
