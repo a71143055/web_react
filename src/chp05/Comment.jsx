face7f85cbcf0e31792ac106dd449a4d694db4ce
a71143055 <139251375+a71143055@users.noreply.github.com>
2024-11-16 17:28:04 +0900

    chp05

---
+++ src/chp05/Comment.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Comment(props) {
+    return(
+        <div>
+            <h1>Comment 컴퍼넌트를 사용했습니다.</h1>
+        </div>
+    );
+}
+
+export default Comment;
